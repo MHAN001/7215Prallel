9e7a4e2a7bd2e6a49da1dc13ecd0876b1bcc3650
MHAN001 <hanxu.workbox@gmail.com>
2018-11-12 11:40:29 -0500

    save changes

---
+++ hw5/Solution/src/cmsc433_p4/StudentMTMazeSolver.java
@@ -0,0 +1,25 @@
+package cmsc433_p4;
+
+import java.util.List;
+
+/**
+ * This file needs to hold your solver to be tested. 
+ * You can alter the class to extend any class that extends cmsc433_p4.MazeSolver.
+ * It must have a constructor that takes in a cmsc433_p4.Maze.
+ * It must have a solve() method that returns the datatype List<cmsc433_p4.Direction>
+ *   which will either be a reference to a list of steps to take or will
+ *   be null if the maze cannot be solved.
+ */
+public class StudentMTMazeSolver extends SkippingMazeSolver
+{
+    public StudentMTMazeSolver(Maze maze)
+    {
+        super(maze);
+    }
+
+    public List<Direction> solve()
+    {
+        // TODO: Implement your code here
+        throw new RuntimeException("Not yet implemented!");
+    }
+}
